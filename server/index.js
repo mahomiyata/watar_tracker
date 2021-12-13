9ce9bbea1b44cd58fc1a00e68fc9cc0ed22f2b7e
Maho Miyata <k.paddy123+github@gmail.com>
2021-12-14 08:52:47 +0900

    fix: テストのためにexpressの設定を切り出す

--- server/index.js
+++ server/index.js
@@ -1,28 +1,4 @@
-const express = require('express');
-const morgan = require('morgan');
-const knex = require('./knex');
-const path = require('path');
-
-const app = express();
-
-// Logger
-app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length]'));
-app.use(express.json());
-
-// static resources
-app.use(express.static(path.join(__dirname, '../dist/')));
-
-// TODO: Delete this endpoint!
-app.get('/api/hi',(req,res) => {
-  res.send('hello!');
-});
-
-app.post('/api/water_amount',async (req, res) => {
-  // Data sent to this endpoint: { amount: 200 }
-  const data = req.body;
-  const result = await knex('amount_of_water').insert(data);
-  res.json({message: 'success'});
-});
+const app = require('./app');
 
 app.listen( process.env.PORT || '4000', () => {
   console.log('Server is running on port 4000!');
