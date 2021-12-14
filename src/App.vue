b8030b3da4da6dd9767e588616a307c6929e2677
Maho Miyata <k.paddy123+github@gmail.com>
2021-12-14 15:23:46 +0900

    feat: 残りのml数を表示 #11

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <input type="text" v-model="amount">
     <button v-on:click="postAmount">Send</button>
+    <p> あと {{restAmount}} ml だよ</p>
   </div>
 </template>
 
@@ -10,8 +11,26 @@ export default {
   name: 'App',
   components: {},
   data: () => ({
-    amount: 0
+    amount: 0,
+    weeklyData: [],
+    restAmount: 0
   }),
+  created: function () {
+    fetch('/api/water_amount/this_week')
+    .then(res => res.json())
+    .then(res => this.weeklyData = res)
+    .then(() => {
+      const today = new Date();
+      const JST = today.toJSON();
+      const regex = new RegExp(/T.*?Z/, 'g');
+      const JSTday = JST.replace(regex, '');
+
+      const todayData = this.weeklyData.filter((item) => {
+        return item.created_at === JSTday;
+      });
+      this.restAmount = 1500 - todayData[0].amount;
+    })
+  },
   methods: {
     postAmount: function() {
       fetch('/api/water_amount',{
