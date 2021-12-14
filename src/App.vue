10411413a34b9d72d142645c25add4a3613de005
Maho Miyata <k.paddy123+github@gmail.com>
2021-12-14 14:42:04 +0900

    feat: 水の量をpostできるようにした

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,27 @@
 <template>
   <div id="app">
-    HEllo!
+    <input type="text" v-model="amount">
+    <button v-on:click="postAmount">Send</button>
   </div>
 </template>
 
 <script>
 export default {
   name: 'App',
-  components: {
-    
+  components: {},
+  data: () => ({
+    amount: 0
+  }),
+  methods: {
+    postAmount: function() {
+      fetch('/api/water_amount',{
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({amount: this.amount})
+      }).then(() => console.log('clicked!'));
+    }
   }
 }
 </script>
